ed0c09f38bb1566be5b82722e02dbd73ad2223dc
iamquell <santanaraquel390@gmail.com>
2022-02-10 22:40:50 -0300

    Realizei um novo exercicio no Vs code

---
+++ modulo1/variaveis/operadores/template_javascript/index.js
@@ -0,0 +1,87 @@
+// Questões interpretativas //
+
+/* Resposta 1. 
+
+A - false 
+B -false
+C - false 
+D - bolean
+*/
+
+// Resposta 2
+
+/* 
+Acredito que ele esqueceu e colocar o *resultado* antes das variaveis de soma// 
+ Sugiro que ele que acrescente o *resultado antes do *soma*
+ */
+
+ /*
+ EXERCÍCIO DE ESCRITA DE CÓDIGO 
+
+ */
+
+// QUESTÃO 1//
+
+const suaIdade = Number(prompt("Qual é sua Idade?"))
+const idadeAmigo = Number(prompt("Qual é a idade do seu melhor amigo?"))
+
+const valor = suaIdade > idadeAmigo
+let idade =  suaIdade - idadeAmigo
+
+console.log("Sua idade é maior do que a do seu melhor amigo?", valor)
+console.log("A diferença de idade é de", idade, "anos")
+
+// QUESTÃO 2 //
+
+// A. AQUI EU SOLICITEI AO USUARIO QUE DIGITASSE UM NÚMERO PAR//
+let numero = Number(prompt("Insira um número par"))
+
+// B.  AQUI FOI DIVIDIDO O NÚMERO PAR QUE O USUARIO DIGITOU POR 2, E RESTO DA DIVISÃO FOI IMPRIMIDO NO CONSOLE//
+const restoDaDivisao = numero % 2
+console.log(restoDaDivisao) 
+
+/* 
+
+C. O padrão que eu notei foi sempre dar 0
+
+
+ D. SE O USUARIO DIGITAR NÚMEROS IMPARES, O RESULTADO VAIS SER 1
+ 
+ */
+
+ // 3. QUESTÃO //
+
+ idade = Number(prompt("Qual a sua idade?"))
+
+ let idadeEmMeses = ("Sua idade em meses é", idade*12)
+ console.log(idadeEmMeses)
+
+
+
+  
+
+ 
+ 
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
